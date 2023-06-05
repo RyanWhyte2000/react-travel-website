6b5d167e50055b629ae0466b63be1daf9890da4b
Ryan whyte <ryanwhytejamaica@gmail.com>
2023-06-05 09:18:29 -0500

    Project continuation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
-      </div>
+    <>
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+    </Router>
+
+    </>
   );
 }
 
